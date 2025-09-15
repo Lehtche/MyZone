67d9710077999396d11c34b727aef2bbd7711825
João Vitor <joaovitor.teixeira@a.ucb.br>
2025-09-15 16:46:39 -0300

    Adicionei Classes de modelo: Avaliação, Colecao e Midia. Adicionei os Getters e Setters na classe Usuario
    Criei novas classes de modelo Avaliação, Coleção e Mídia com campos e métodos getter/setter apropriados. Também conclui os métodos getter e setter na classe Usuário.

---
+++ src/main/java/mycompany/myzone/model/Avaliacao.java
@@ -0,0 +1,48 @@
+package mycompany.myzone.model;
+
+public class Avaliacao {
+    private int id;
+    private Usuario Usuario;
+    private Midia midia;
+    private int nota;
+    private String comentario;
+    private String dataAvaliacao;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Usuario getUsuario() {
+        return Usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.Usuario = usuario;
+    }
+    public Midia getMidia() {
+        return midia;
+    }
+    public void setMidia(Midia midia) {
+        this.midia = midia;
+    }
+    public int getNota() {
+        return nota;
+    }
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+    public String getComentario() {
+        return comentario;
+    }
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    public String getDataAvaliacao() {
+        return dataAvaliacao;
+    }
+    public void setDataAvaliacao(String dataAvaliacao) {
+        this.dataAvaliacao = dataAvaliacao;
+    }
+}
+
---
+++ src/main/java/mycompany/myzone/model/Midia.java
@@ -0,0 +1,49 @@
+package mycompany.myzone.model;
+
+public class Midia {
+    private int id;
+    private String titulo;
+    private String tipo;
+    private int ano;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
